0291bb32ed1bbe28384ec4e6c757bec6af7fd3d6
Sebastian Morr <sebastian@morr.cc>
2012-04-16 12:26:34 +0200

    Unveränderte Sources, Grundgerüst eines LaTeX-Dokuments, vernünftige .gitignore

---
+++ 01/code/include/Ex01.h
@@ -0,0 +1,8 @@
+#ifndef __EX01__
+#define __EX01__
+
+#include <glm/glm.hpp>
+#include <cmath>
+#include <iostream>
+
+#endif
---
+++ 01/code/src/Ex01.cpp
@@ -0,0 +1,12 @@
+#include "Ex01.h"
+
+void triangleSolution(void) {
+  // TODO: implement your solution here //
+  
+}
+
+int main (int argc, char **argv) {
+  triangleSolution();
+  
+  return 0;
+}
