86aa89706c39f15ae100ae0455ad74a3369845af
Sebastian Morr <sebastian@morr.cc>
2012-04-20 18:23:38 +0200

    Programmieraufgabe gelöst

--- 01/code/src/Ex01.cpp
+++ 01/code/src/Ex01.cpp
@@ -1,8 +1,23 @@
 #include "Ex01.h"
 
+using namespace glm;
+using namespace std;
+
 void triangleSolution(void) {
-  // TODO: implement your solution here //
-  
+    // Die vier Vektoren aus der Aufgabe:
+    vec3 a(4,1,0);
+    vec3 b(-1,3,2);
+    vec3 c(0,2,1);
+    vec3 v(1,1,-3);
+
+    // Normale in Richtung des Betrachters (in einem rechtshändigen Koordinatensystem)
+    vec3 n = normalize(cross(b-a, c-a)); 
+
+    // Hierfür scheint in glm keine dedizierte Funktion zu existieren:
+    float angle = acos(dot(n,v)/(length(n)*length(v)));
+
+    cout << "n = (" << n.x << " " << n.y << " " << n.z << ")\n";
+    cout << "angle = " << angle << " rad = " << degrees(angle) << " degrees \n";
 }
 
 int main (int argc, char **argv) {
