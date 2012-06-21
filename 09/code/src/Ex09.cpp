d27e1210772ad258f5a0dfe374f8399221553edf
Marc Kassubeck <marc.kassubeck@googlemail.com>
2012-06-21 17:59:09 +0200

    Uniform locations hinzugefügt

--- 09/code/src/Ex09.cpp
+++ 09/code/src/Ex09.cpp
@@ -355,7 +355,7 @@ void initShader() {
 			return;
 		}
 
-		// TODO: set output format for both shaders (hint: glBindFragDataLocation) //
+		// TODO?: set output format for both shaders (hint: glBindFragDataLocation) //
 		glBindFragDataLocation(shaderPass[0], 0, "vertex_pos");
 		glBindFragDataLocation(shaderPass[0], 1, "vertex_normal");
 		glBindFragDataLocation(shaderPass[0], 2, "vertex_texcoord");
@@ -630,9 +630,9 @@ void initFBO() {
 	createEmptyTexture("texcoord", windowWidth, windowHeight);
 
 	// TODO: get uniforms locations in shader (pass 0) //
-	uniformLocations["def_vertexMap"] = glGetUniformLocation(shaderPass[1], "def_vertexMap");
-	uniformLocations["def_normalMap"] = glGetUniformLocation(shaderPass[1], "def_normalMap");
-	uniformLocations["def_texCoordMap"] = glGetUniformLocation(shaderPass[1], "def_texCoordMap");
+	textures["position"].uniformLocation = glGetUniformLocation(shaderPass[1], "def_vertexMap");
+	textures["normal"].uniformLocation = glGetUniformLocation(shaderPass[1], "def_normalMap");
+	textures["texcoord"].uniformLocation = glGetUniformLocation(shaderPass[1], "def_texCoordMap");
 
 	// TODO: attach textures to FBO for output VERTEX, NORMAL, TEXCOORD //
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textures["position"].uniformLocation, 0);
